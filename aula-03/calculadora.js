d73f2705dd9a51ef8d9ce6ae637282dff9c898b7
LAYLA GIOVANNA FURTADO DA SILVA <22282096@CFP127ALUNO.LOCAL>
2023-02-09 09:47:53 -0300

    fazer ajustes depois

---
+++ aula-03/calculadora.js
@@ -0,0 +1,122 @@
+/************************
+ * Objetivo: Arquivos de funções paraa realizar calculos matematicos
+ * Data: 03/02/2023
+ * Autora: 
+ * Versão: 1.0
+***********************/
+
+//Realizar calculos matemáticos para realizar as 4 operações (SOMAR, SUBTRAIR, DIVIDIR, MULTIPLICAR)
+//forma 01 (tradicional)
+/* function calcular(numero1, numero2, tipoCalculo) {
+
+    let valor1 = Number(numero1);
+    let valor2 = Number(numero2);
+    let operacao = tipoCalculo.toUpperCase();
+
+    let resultado;
+    let status = true;
+
+    // if (operacao == 'SOMAR') {
+    //     resultado = valor1 + valor2;
+    // } else if (operacao == 'SUBTRAIR') {
+    //     resultado = valor1 - valor2;
+    // } else if (operacao == 'MULTIPLICAR') {
+    //     
+    // } else if (operacao == 'DIVIDIR') {
+    //     if (valor2 == 0) {
+    //         console.log('ERRO: Não foi possível fazer uma divisão por zero')
+    //         status = false;
+    //     } else {
+    //         resultado = valor1 / valor2;
+    //     } else {
+    //     console.log('ERRO: A operação informada não é válida, confira a sua entrada. \n')
+    //     status = false;
+    // }
+
+    switch (operacao){
+        case 'SOMAR':
+            resultado = valor1 + valor2;
+            break;
+        case 'SUBTRAIR':
+            resultado = valor1 - valor2;
+            break;
+        case 'MULTIPLICAR':
+            resultado = valor1 * valor2;
+            break;
+        case 'DIVIDIR':
+            if (valor2 == 0) {
+                console.log('ERRO: Não foi possível fazer uma divisão por zero')
+                status = false;
+            } else 
+                resultado = valor1 / valor2;
+
+                break;
+        default:
+            console.log('ERRO: A operação informada não é válida, confira a sua entrada. \n')
+            status = false;
+    }
+
+    //validação para tratar a variável resultado quando nenhum calculo é realizado
+    if (resultado != undefined)
+        return resultado;
+    else {
+        status = false;
+    }
+
+}; */
+
+//forma 02 de criar funçao
+const calcular = function (numero1, numero2 , tipoCalculo){
+    //forma 01 (tradicional)
+function calcular(numero1, numero2, tipoCalculo) {
+
+    let valor1 = Number(numero1);
+    let valor2 = Number(numero2);
+    let operacao = tipoCalculo.toUpperCase();
+
+    let resultado;
+    let status = true;
+
+
+    switch (operacao){
+        case 'SOMAR':
+            resultado = valor1 + valor2;
+            break;
+        case 'SUBTRAIR':
+            resultado = valor1 - valor2;
+            break;
+        case 'MULTIPLICAR':
+            resultado = valor1 * valor2;
+            break;
+        case 'DIVIDIR':
+            if (valor2 == 0) {
+                console.log('ERRO: Não foi possível fazer uma divisão por zero')
+                status = false;
+            } else 
+                resultado = valor1 / valor2;
+
+                break;
+        default:
+            console.log('ERRO: A operação informada não é válida, confira a sua entrada. \n')
+            status = false;
+    }
+
+    //validação para tratar a variável resultado quando nenhum calculo é realizado
+    if (resultado != undefined)
+        return resultado.toFixed(2);
+    else {
+        status = false;
+    }
+
+};
+}
+
+//forma 03
+const SOMAR = (valor1, valor2) => valor1 + valor2;
+const SUBTRAIR = (valor1, valor2) => valor1 - valor2;
+const MULTIPLICAR = (valor1, valor2) => valor1 * valor2;
+const DIVIDIR = (valor1, valor2) => valor1 / valor2;
+
+module.exports = {
+    calcular
+}
---
+++ aula-03/calculadora/app.js
@@ -0,0 +1,65 @@
+/*
+ * objetivo: Realizar calculos matematicos(SOMA, SUBTRAÇAO, MULTIPLICACAO E DIVISAO)
+ * DATA:03/02/2023
+ * AUTOR: paula
+ * versao: 1.0
+ */
+
+ //Import da biblioteca da calculadora (criado por nos)
+ var matematica = require('./modulo/calculadora.js')
+
+ //Import da biblioteca para entrada de dados
+ var readline = require('readline')
+ 
+ //criar um objeto para receber os dados via teclado
+ var entradaDados = readline.createInterface({
+     input: process.stdin,
+     output: process.stdout
+ })
+ 
+ entradaDados.question('valor1: \n', function(numero1) {
+     //replace -permite trocar um conteudo da string por outro
+     let valor1 = numero1.replace(',', '.');
+ 
+     entradaDados.question('valor2: \n', function(numero2) {
+ 
+         let valor2 = numero2.replace(',', '.');
+ 
+         entradaDados.question('Escolha uma operação: [ SOMAR | SUBTRAIR | MULTIPLICAR | DIVIDIR ]: \n', function(tipoCalculo) {
+ 
+             //toUpperCase - converte uma string para MAIUSCULO
+             //toLowerrCase - converte uma string para minusculo
+             let operacao = tipoCalculo.toUpperCase();
+ 
+             let resultado;
+ 
+             //Validacao de entrada de dados vazio
+             //se usar o typeof, precisa garantir que o tipo de dados nao é generico (string)
+ 
+             if (valor1 == '' || valor2 == '' || operacao == '') {
+                 console.log('ERRO: Não é possivel calcular sem preencheer todos os dados.')
+                 entradaDados.close();
+ 
+             } else if (isNaN(valor1) || isNaN(valor2)) {
+                 console.log('ERRO: Não é possivel calcular sem a entrada de valores numéricos.')
+                 entradaDados.close(); // fecha o objeto de entrada de dados (encerra o programa)
+             } else {
+ 
+                 //chama a funçao para calcular, encaminhando os dados para o calculo
+                 resultado = matematica.calcular(valor1, valor2, operacao);
+ 
+                 //validar se o retorno da funcao é verdadeira ou falso (se for falso, encerra o programa)
+                 //if (resultado == false && typeof(resultado) == 'boolean') ou
+                 if(resultado === false )
+                  entradaDados.close();
+ 
+                else 
+                     console.log(resultado);
+                 
+ 
+ 
+             }
+         })
+     })
+ 
+ })
